7e5e6f7a4f631718e4e08af58c3e5c6191b32137
Achun1130 <58815675+Achun-1130@users.noreply.github.com>
2020-08-30 01:38:54 +0800

    +

--- js/all.js
+++ js/all.js
@@ -3,7 +3,17 @@ $(document).ready(function () {
         event.preventDefault();
         $('body').toggleClass('active');
     });
-    $('.nav li a').on('click', function (event) {
+    $(".nav li a[href^='#']").on('click', function (event) {
+        event.preventDefault();
         $('body').removeClass('active');
+    });
+    
+    $(".nav li a[href^='#']").on('click', function (event) {
+        event.preventDefault();
+        var href = $(this).attr('href');
+        console.log(href);
+        $('html,body').animate({
+            scrollTop: $(href).offset().top-52
+        },1000);
     });
 });
