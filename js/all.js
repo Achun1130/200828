e52a701c565f454fa1035d66a508352f97852116
Achun1130 <58815675+Achun-1130@users.noreply.github.com>
2020-08-31 22:45:23 +0800

    +

--- js/all.js
+++ js/all.js
@@ -1,19 +1,33 @@
 $(document).ready(function () {
+    // cart 選單
+    $('.open').on('click', function (event) {
+        event.preventDefault();
+        $(this).parent().toggleClass('active');
+    });
+    // pad & mobile 選單
     $('.nav-btn').on('click', function (event) {
         event.preventDefault();
         $('body').toggleClass('active');
+        // cart 選單收
+        $('.open').parent().removeClass('active');
     });
+    
+    // 移至 index 指定位置
     $(".nav li a[href^='#']").on('click', function (event) {
         event.preventDefault();
         $('body').removeClass('active');
-    });
-    
-    $(".nav li a[href^='#']").on('click', function (event) {
-        event.preventDefault();
+        // cart 選單收
+        $('.open').parent().removeClass('active');
         var href = $(this).attr('href');
-        console.log(href);
+        // console.log(href);
         $('html,body').animate({
             scrollTop: $(href).offset().top-52
         },1000);
+    });
+
+    // cart-item 購物車
+    $('.cart-item li a').on('click', function (event) {
+        event.preventDefault();
+        $(this).parent().toggleClass('active');   
     });
 });
